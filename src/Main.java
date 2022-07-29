016b9e8194b1f2585560770df57687c4561b263a
Pavilion <kirill24-974@mail.ru>
2022-07-29 14:06:27 +0300

    задача1.2

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        // задание1
+        int dog = 5;
+        short mouse = 4;
+        long elephant = 123L;
+        byte igor = 2;
+        float sugarWeight = 2.3f;
+        double saltWeight = 3.2;
+        char c = 33;
+        char d = '!';
+        System.out.println(c + " = " + d);
+
+        // Задание2
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double weightOfAllBoxer = boxer1 + boxer2;
+        double weightDifference = boxer2 - boxer1;
+        System.out.println( + weightOfAllBoxer);
+        System.out.println( - weightDifference);
+
+        // Задача 3
+        int Bananas = 5;
+        int OneBanana = 80;
+        System.out.println(Bananas * OneBanana);
+        int milk200ml = 2;
+        int milk100 =105;
+        System.out.println(milk200ml * milk100);
+        int IceCreamSundae = 2;
+        int IceCreamSundae1Briket =100;
+        System.out.println(IceCreamSundae * IceCreamSundae1Briket);
+        int RawEggs =4;
+        int oneEgg =70;
+        System.out.println(RawEggs * oneEgg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
